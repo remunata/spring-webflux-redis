74f4c8a8d6f572bc1237646ccad267f685b8d4f6
Mahendra Dinata <remdinata@gmail.com>
2023-09-18 23:13:08 +0700

    init project

---
+++ src/test/java/remunata/springwebfluxredis/SpringWebfluxRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package remunata.springwebfluxredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
